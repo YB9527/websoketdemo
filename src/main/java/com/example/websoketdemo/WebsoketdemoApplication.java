9d93d454f00c2ab4ee460abe9516eb0e3181b7e4
LS <260890728@qq.com>
2021-05-27 17:50:21 +0800

    Initial commit

---
+++ src/main/java/com/example/websoketdemo/WebsoketdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.websoketdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsoketdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsoketdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/websoketdemo/WebsoketdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websoketdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsoketdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
